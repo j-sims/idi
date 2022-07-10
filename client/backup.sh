95004a529382024784b845e820353743a8299e48
j-sims <jim.sims@nowhere.com>
2022-07-10 10:26:27 -0400

    backup/upload adjustments

--- client/backup.sh
+++ client/backup.sh
@@ -2,8 +2,7 @@ WORKDIR=/working
 TS=`date "+%Y%m%d-%H%M%S"`
 [[ ! -d $WORKDIR ]] && mkdir -p $WORKDIR
 [[ `find $WORKDIR -maxdepth 0 -empty` == "" ]] && rm -f $WORKDIR/*
-/usr/bin/influxd backup -database isi_data_insights -host influxdb:8088 $WORKDIR && \
-tar cf $WORKDIR/backup-$TS.tar /backups && \
-cp $WORKDIR/backup-$TS.tar /backups && \
-gpg --output /backups/backups.gpg --encrypt --recipient "Dell UDS CAE" $WORKDIR/backup-$TS.tar && \
-[[ `find $WORKDIR -maxdepth 0 -empty` == "" ]] && rm -f $WORKDIR/*
+/usr/bin/influxd backup -portable -database isi_data_insights -host influxdb:8088 $WORKDIR && \
+tar cf /backups/backup-$TS.tar $WORKDIR && \
+gpg --output /backups/latestbackup.gpg --encrypt --recipient "Dell UDS CAE" /backups/backup-$TS.tar && \
+echo success
--- client/run.sh
+++ client/run.sh
@@ -1,7 +1,7 @@
 bash /backup.sh && \
 	echo "Backup Successful" && \
 	if [[ "$UPLOAD" == "true" ]]; then
-	python3 upload.py /backups/backups.gpg && \
+	python3 upload.py /backups/latestbackup.gpg && \
 	echo "Upload Successful"
 	else
 	rm /backups/backups.gpg
