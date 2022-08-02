87091a4e92e302d18fc55a22c1a83b7e28ef454c
j-sims <jim.sims@nowhere.com>
2022-08-02 12:07:36 -0500

    add version to the upload

--- client/backup.sh
+++ client/backup.sh
@@ -3,6 +3,7 @@ TS=`date "+%Y%m%d-%H%M%S"`
 [[ ! -d $WORKDIR ]] && mkdir -p $WORKDIR
 [[ `find $WORKDIR -maxdepth 0 -empty` == "" ]] && rm -f $WORKDIR/*
 /usr/bin/influxd backup -portable -database isi_data_insights -host influxdb:8088 $WORKDIR && \
+echo $VERSION > $WORKDIR/VERSION && \
 tar cf /backups/backup-$TS.tar $WORKDIR && \
 gpg --output /backups/latestbackup.gpg --encrypt --recipient "Dell UDS CAE" /backups/backup-$TS.tar && \
 echo success
